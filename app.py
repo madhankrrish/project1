ac64f400cf48f3520fc5761bf6a8d5638bd55cc9
madhankrrish <48155136+madhankrrish@users.noreply.github.com>
2019-09-25 17:30:35 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+import os
+bot=ChatBot('trainmaster')
+trainer=ListTrainer(bot)
+for knowledeg in os.listdir('base'):
+
+	BotMemory = open('base/'+ knowledeg, 'r').readlines()
+	trainer.train(BotMemory)
 
 app = Flask(__name__)
-
-english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
-trainer = ChatterBotCorpusTrainer(english_bot)
-trainer.train("chatterbot.corpus.english")
-
+arr=[]
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -15,8 +18,13 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+    f = open("backup.txt", "a+")
 
+    arr.append(userText)
+    for i in arr:
+        f.write(i + "\n")
 
+    f.close()
+    return str(bot.get_response(userText))
 if __name__ == "__main__":
     app.run()
